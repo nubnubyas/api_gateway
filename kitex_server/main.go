f9f468982129af30ea3e36da4acb84140e2c15ba
nubnubyas <zhangyiheng0421@gmail.com>
2023-06-15 21:43:15 +0800

    kitex server up

---
+++ kitex_server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	api "github.com/cloudwego/api_gateway/kitex_server/kitex_gen/api/studentapi"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+
+	"github.com/cloudwego/kitex/server"
+	"github.com/kitex-contrib/registry-nacos/registry"
+)
+
+func main() {
+	r, err := registry.NewDefaultNacosRegistry()
+
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	svr := api.NewServer(
+		&StudentApiImpl{},
+		server.WithRegistry(r),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "payment"}),
+		server.WithServiceAddr(&net.TCPAddr{Port: 8081}),
+	)
+
+	err = svr.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
